d8139fcccd4366a5677ad72646fde878948b780c
Danaé A.M. <dev.danae@gmail.com>
2025-05-16 15:23:28 +0200

    CHG : Update logs for commands usage

--- src/utils/logs_utils.py
+++ src/utils/logs_utils.py
@@ -1,2 +1,5 @@
+from datetime import datetime
+
 def log_command_usage(command_name: str, user_id: str, user_nickname: str, user_lang: str) -> None:
-    print(f"Commande {command_name} appelée par {user_nickname} ({user_id}) [{user_lang}]")
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{current_time}] Commande {command_name} appelée par {user_nickname} ({user_id}) [{user_lang}]")
